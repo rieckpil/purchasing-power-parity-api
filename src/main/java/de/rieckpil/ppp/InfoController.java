a696a0aad4b993bad51989eff78439b490011b05
Philip Riecks <mail@philipriecks.de>
2024-04-07 14:28:32 +0200

    prometheus

--- src/main/java/de/rieckpil/ppp/InfoController.java
+++ src/main/java/de/rieckpil/ppp/InfoController.java
@@ -1,5 +1,6 @@
 package de.rieckpil.ppp;
 
+import io.micrometer.core.annotation.Counted;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InfoController {
 
   @GetMapping
+  @Counted(value = "info.count", description = "How often the info endpoint was called")
   public String getInfo() {
     return "info";
   }
