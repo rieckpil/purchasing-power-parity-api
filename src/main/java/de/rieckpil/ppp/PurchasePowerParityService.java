062f25f46213e248c136d9dcbfdca8f80f4c38c4
Philip Riecks <mail@philipriecks.de>
2024-04-07 12:04:21 +0200

    some tests

--- src/main/java/de/rieckpil/ppp/PurchasePowerParityService.java
+++ src/main/java/de/rieckpil/ppp/PurchasePowerParityService.java
@@ -1,5 +1,6 @@
 package de.rieckpil.ppp;
 
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -7,6 +8,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class PurchasePowerParityService {
   public Optional<PurchasePowerParityResponsePayload> getByCountryCode(String countryCode) {
+
+    if (countryCode.equals("DE")) {
+      return Optional.of(
+          new PurchasePowerParityResponsePayload("DE", "DEU", Map.of("EUR", "Germany"), 1.0, 1.0));
+    }
     return Optional.empty();
   }
 }
--- src/test/java/de/rieckpil/ppp/ApplicationIT.java
+++ src/test/java/de/rieckpil/ppp/ApplicationIT.java
@@ -1,21 +1,42 @@
 package de.rieckpil.ppp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 @Testcontainers
-@SpringBootTest
-class ApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApplicationIT {
+
+  @Autowired private WebTestClient webTestClient;
 
   @Container @ServiceConnection
   static PostgreSQLContainer<?> postgresContainer =
       new PostgreSQLContainer<>(DockerImageName.parse("postgres:15:1"));
 
   @Test
-  void contextLoads() {}
+  void hitHealth() {
+    webTestClient.get().uri("/actuator/health").exchange().expectStatus().isOk();
+  }
+
+  @Test
+  void hitPurchasePowerParityApi() {
+    webTestClient.get().uri("/?target=DE").exchange().expectStatus().isOk();
+  }
+
+  @Test
+  void rejectPurchasePowerParityApi() {
+    webTestClient.get().uri("/").exchange().expectStatus().isBadRequest();
+  }
+
+  @Test
+  void notFoundPurchasePowerParityApi() {
+    webTestClient.get().uri("/?target=NOT_FOUND").exchange().expectStatus().isNotFound();
+  }
 }
