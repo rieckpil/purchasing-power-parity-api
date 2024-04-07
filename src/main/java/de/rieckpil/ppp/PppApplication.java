3e563707cae3f0babe8e81a533195e25dc69a465
Philip Riecks <mail@philipriecks.de>
2024-04-07 10:23:47 +0200

    initial commit

---
+++ src/main/java/de/rieckpil/ppp/PppApplication.java
@@ -0,0 +1,13 @@
+package de.rieckpil.ppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/rieckpil/ppp/PppApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rieckpil.ppp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
