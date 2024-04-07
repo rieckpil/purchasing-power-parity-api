31d47420e57217e25016011eadce211b406f0a21
Philip Riecks <mail@philipriecks.de>
2024-04-07 12:34:23 +0200

    enrich

--- src/main/java/de/rieckpil/ppp/SecurityConfig.java
+++ src/main/java/de/rieckpil/ppp/SecurityConfig.java
@@ -2,7 +2,10 @@ package de.rieckpil.ppp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -10,7 +13,12 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
+    http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+        .csrf(AbstractHttpConfigurer::disable)
+        .httpBasic(Customizer.withDefaults())
+        .sessionManagement(
+            sessionManagement ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
   }
