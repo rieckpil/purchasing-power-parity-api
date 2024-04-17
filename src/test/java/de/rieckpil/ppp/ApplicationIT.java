5bbbb0ebc3e5d95692add7b53487e9dcf0588ff7
Philip Riecks <mail@philipriecks.de>
2024-04-17 15:42:56 +0300

    temp tc for jooq

--- src/test/java/de/rieckpil/ppp/ApplicationIT.java
+++ src/test/java/de/rieckpil/ppp/ApplicationIT.java
@@ -4,7 +4,8 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -17,9 +18,18 @@ class ApplicationIT {
 
   @Autowired private WebTestClient webTestClient;
 
-  @Container @ServiceConnection
+  @Container
   static PostgreSQLContainer<?> postgresContainer =
-      new PostgreSQLContainer<>(DockerImageName.parse("postgres:15:1"));
+      new PostgreSQLContainer<>(DockerImageName.parse("postgres:15.1-alpine"));
+
+  @DynamicPropertySource
+  static void configure(DynamicPropertyRegistry dynamicPropertyRegistry) {
+    dynamicPropertyRegistry.add(
+        "spring.r2dbc.url", () -> "r2dbc:" + postgresContainer.getJdbcUrl());
+    dynamicPropertyRegistry.add("spring.r2dbc.username", () -> postgresContainer.getUsername());
+    dynamicPropertyRegistry.add("spring.r2dbc.password", () -> postgresContainer.getPassword());
+    dynamicPropertyRegistry.add("spring.flyway.url", () -> postgresContainer.getJdbcUrl());
+  }
 
   @Test
   void hitHealth() {
