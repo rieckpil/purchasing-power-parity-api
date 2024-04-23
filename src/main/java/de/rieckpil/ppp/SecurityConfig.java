31618420c2cca8c475bb41dd86dabda309e3abcc
Philip Riecks <mail@philipriecks.de>
2024-04-23 11:08:58 +0200

    fix

--- src/main/java/de/rieckpil/ppp/SecurityConfig.java
+++ src/main/java/de/rieckpil/ppp/SecurityConfig.java
@@ -2,13 +2,8 @@ package de.rieckpil.ppp;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -18,7 +13,7 @@ public class SecurityConfig {
   @Bean
   public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
     http.authorizeExchange(requests -> requests.anyExchange().permitAll())
-      .csrf(ServerHttpSecurity.CsrfSpec::disable);
+        .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
     return http.build();
   }
