0250b06cb58953e105f697b30b71e26fe066e7d4
Philip Riecks <mail@philipriecks.de>
2024-04-23 12:02:16 +0200

    cors

---
+++ src/main/java/de/rieckpil/ppp/ServerConfig.java
@@ -0,0 +1,16 @@
+package de.rieckpil.ppp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+public class ServerConfig implements WebFluxConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET").allowedHeaders("*");
+  }
+}
