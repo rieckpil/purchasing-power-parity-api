71e7139db8c1f545e5fc245f70ed6416778c25e2
Philip Riecks <mail@philipriecks.de>
2024-04-23 11:06:57 +0200

    no web needed

--- src/main/java/de/rieckpil/ppp/SecurityConfig.java
+++ src/main/java/de/rieckpil/ppp/SecurityConfig.java
@@ -5,20 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
-        .csrf(AbstractHttpConfigurer::disable)
-        .httpBasic(Customizer.withDefaults())
-        .sessionManagement(
-            sessionManagement ->
-                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+  public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+    http.authorizeExchange(requests -> requests.anyExchange().permitAll())
+      .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
     return http.build();
   }
